7c7043ff12b4c01a93c60a3643f9f2a99d13d518
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-25 12:02:45 +0100

    Updated mobile menu, made hero image responsive

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var menuButtons = document.querySelectorAll('[data-sub]');
 var closeMenuBtn = document.querySelector('.close-btn');
 var nav = document.querySelector('nav');
 var menuToggleBtn = document.querySelector('.menu-toggle');
+var heroImg = document.querySelector('.hero__img');
 menuButtons.forEach(function (button) {
     return button.addEventListener('click', function (e) {
         button.classList.toggle('expanded');
@@ -21,3 +22,14 @@ function toggleArrow(el) {
         ? './images/icon-arrow-down.svg'
         : './images/icon-arrow-up.svg';
 }
+// function changeImageForSize() {
+//   // const desktopSize = getComputedStyle(document.body)
+//   //   .getPropertyValue('--desktop-width')
+//   //   .split('px')[0];
+//   const heroImage: HTMLImageElement = document.querySelector('.hero__img');
+//   heroImage.src =
+//     window.innerWidth >= 700
+//       ? './images/image-hero-desktop.png'
+//       : (heroImage.src = './images/image-hero-mobile.png');
+// }
+// window.onresize = changeImageForSize;
--- script.ts
+++ script.ts
@@ -1,9 +1,10 @@
 const menuButtons = document.querySelectorAll('[data-sub]');
-const closeMenuBtn = document.querySelector('.close-btn');
-const nav = document.querySelector('nav');
+const closeMenuBtn = document.querySelector('.close-btn') as HTMLElement;
+const nav = document.querySelector('nav') as HTMLElement;
 const menuToggleBtn: HTMLElement = document.querySelector('.menu-toggle');
+const heroImg = document.querySelector('.hero__img');
 
-menuButtons.forEach((button) =>
+menuButtons.forEach((button: HTMLLIElement) =>
   button.addEventListener('click', (e) => {
     button.classList.toggle('expanded');
     toggleArrow(e.target);
@@ -25,3 +26,16 @@ function toggleArrow(el: EventTarget): void {
     ? './images/icon-arrow-down.svg'
     : './images/icon-arrow-up.svg';
 }
+
+// function changeImageForSize() {
+//   // const desktopSize = getComputedStyle(document.body)
+//   //   .getPropertyValue('--desktop-width')
+//   //   .split('px')[0];
+//   const heroImage: HTMLImageElement = document.querySelector('.hero__img');
+//   heroImage.src =
+//     window.innerWidth >= 700
+//       ? './images/image-hero-desktop.png'
+//       : (heroImage.src = './images/image-hero-mobile.png');
+// }
+
+// window.onresize = changeImageForSize;
